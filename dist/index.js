eafd3836a50104450d5108480c98e4027bc43bfa
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-02-29 14:42:56 +0600

    8 - Tuples

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hello ninjas");
+// tuples
+let person = ['mario', 30, true];
+// tuples examples
+let hsla;
+hsla = [200, '100%', '50%', 1];
+let xy;
+xy = [23.4, 45.6];
+function useCoords() {
+    const lat = 100;
+    const long = 50;
+    return [lat, long];
+}
+const [lat, long] = useCoords();
+// named tuples
+let user;
+user = ["peach", 25];
+console.log(user[0]);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("hello ninjas")
+// tuples
+
+let person: [string, number, boolean] = ['mario', 30, true]
+
+// tuples examples
+
+let hsla: [number, string, string, number]
+
+hsla = [200, '100%', '50%', 1]
+
+let xy: [number, number]
+
+xy = [23.4, 45.6]
+
+function useCoords(): [number, number] {
+    const lat = 100
+    const long = 50
+
+    return [lat, long]
+}
+
+const [lat, long] = useCoords()
+
+// named tuples
+
+let user: [name: string, age: number]
+
+user = ["peach", 25]
+
+console.log(user[0])
