a725262bae7000d35fac652d41195f5626de9fec
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-03-01 19:07:52 +0500

    38 - Querying the DOM

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,34 @@
-import { Pizza } from "./models/Pizza";
+import {Pizza, PizzaProps} from "./models/Pizza";
+
+const rootElement = document.querySelector('.root')!
+
+function createPizzaTemplate(pizza: PizzaProps): string {
+    return `
+      <div class="pizza">
+        <h2>${pizza.title}</h2>
+        <p class="toppings">${pizza.toppings.join(', ')}</p>
+        <p>${pizza.description}</p>
+        <span>$${pizza.price}</span>
+      </div>
+    `
+}
+
+function renderTemplates(templates: string[], parent: Element): void {
+    const templateElement = document.createElement('template')
+
+    for (const t of templates) {
+        templateElement.innerHTML += t
+    }
+
+    parent.append(templateElement.content)
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
     const pizzas = await Pizza.loadAll()
 
-    console.log(pizzas)
+    // create template string for each pizza
+    const pizzaTemplates = pizzas.map(createPizzaTemplate)
+
+    //  render pizza templates to DOM
+    renderTemplates(pizzaTemplates, rootElement)
 })
