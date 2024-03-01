bd2ad16057bd13fa9cdf2ab4f652fcec3cfa5337
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-03-01 14:53:03 +0500

    27 - Generic Interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,16 @@
 "use strict";
-// Intersection Type
-function addIdToValue(val) {
-    const id = Math.random();
-    return Object.assign(Object.assign({}, val), { id });
+// Generic Interface
+const collectionOne = {
+    data: ['mario', 'luigi', 'peach'],
+    name: 'mario characters'
+};
+const collectionTwo = {
+    data: [10, 2, 3, 4],
+    name: 'winning lottery numbers',
+};
+function randomCollectionItem(c) {
+    const idx = Math.floor(Math.random() * c.data.length);
+    return c.data[idx];
 }
-const post = addIdToValue({ title: 'Marmite Rules', thumbsUp: 250 });
-console.log(post.id);
+console.log(randomCollectionItem(collectionOne));
+console.log(randomCollectionItem(collectionTwo));
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,25 @@
-// Intersection Type
+// Generic Interface
 
-interface HasID {
-    id: number
+interface Collection<T> {
+    data: T[]
+    name: string
 }
 
-function addIdToValue<T>(val: T): T & HasID {
-    const id = Math.random()
-
-    return { ...val, id }
+const collectionOne: Collection<string> = {
+    data: ['mario', 'luigi', 'peach'],
+    name: 'mario characters'
 }
 
-interface Post {
-    title: string
-    thumbsUp: number
+const collectionTwo: Collection<number> = {
+    data: [10,2,3,4],
+    name: 'winning lottery numbers',
 }
 
-const post = addIdToValue<Post>({ title: 'Marmite Rules', thumbsUp: 250 })
+function randomCollectionItem<T>(c: Collection<T>): T {
+    const idx = Math.floor(Math.random() * c.data.length)
 
-console.log(post.id)
+    return c.data[idx]
+}
 
-
-
-
-
-
-
-
-
+console.log(randomCollectionItem<string>(collectionOne))
+console.log(randomCollectionItem(collectionTwo))
