32632d2869407a1efacfcceee8bdfb093329bcb4
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-03-01 19:20:46 +0500

    39 - Type Assertion

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,1 +1,14 @@
+const titleInput = document.querySelector(
+    'input[name="title"]'
+) as HTMLInputElement
 
+const descriptionInput = document.querySelector(
+    'textarea'
+)
+
+const form: HTMLFormElement = document.querySelector('.create')!
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+})
