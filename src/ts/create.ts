90a7b1d12469ff876f46178215bfce91da86c441
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-03-01 19:28:32 +0500

    40 - Form Data

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,3 +1,6 @@
+import { PizzaProps } from "./models/Pizza";
+import { Pizza } from "./models/Pizza";
+
 const titleInput = document.querySelector(
     'input[name="title"]'
 ) as HTMLInputElement
@@ -11,4 +14,22 @@ const form: HTMLFormElement = document.querySelector('.create')!
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
+    const data = new FormData(form)
+
+    const newPizza: PizzaProps = {
+        title: data.get('title') as string,
+        description: data.get('description') as string,
+        toppings: data.getAll('toppings') as string[],
+        price: parseInt(data.get('price') as string),
+    }
+
+    const res = await Pizza.save(newPizza)
+
+    if (!res.ok) {
+        console.log('not able to save the pizza')
+    }
+
+    if (res.ok) {
+        window.location.href = '/'
+    }
 })
